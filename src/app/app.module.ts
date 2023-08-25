cada800f1a67818de4f33077ee7b5389fea01e76
nithesh56 <nitheshback06@gmail.com>
2023-08-25 17:23:36 +0530

    no changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './propert/property-card/property-card.component';
+import { PropertyListComponent } from './propert/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
