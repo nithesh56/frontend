b179d240d3bad5a7c2a13af45d12744a2135883d
nithesh56 <nitheshback06@gmail.com>
2023-09-19 12:41:20 +0530

    add

--- src/app/propert/property-card/property-card.component.ts
+++ src/app/propert/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,7 @@ import { Component } from '@angular/core';
 })
 export class PropertyCardComponent {
 
-  property : any ={
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
+  @Input() property : any
 
-  }
 
 }
--- src/app/propert/property-list/property-list.component.ts
+++ src/app/propert/property-list/property-list.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent {
+  properties : Array <any> =[
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
 
+    },
+    {
+      "Id":2,
+      "Name":"Erose villa",
+      "Type":"House",
+      "Price":18600
+
+    },
+    {
+      "Id":3,
+      "Name":"Gree Garden",
+      "Type":"House",
+      "Price":17000
+
+    },
+    {
+      "Id":4,
+      "Name":"White House",
+      "Type":"House",
+      "Price":25000
+
+    },
+    {
+      "Id":5,
+      "Name":"Macro Home",
+      "Type":"House",
+      "Price":20000
+
+    },
+    {
+      "Id":6,
+      "Name":"MoonLigt",
+      "Type":"House",
+      "Price":15000
+
+    },
+
+]
 }
